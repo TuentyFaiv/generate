21ef2e4738e4f419377bb1b8c43e3e14f15dd0b9
Tonalli López <aguilablanca_7@live.com>
2023-03-15 11:57:23 -0600

    🩹 FIX: React project creation on windows

--- src/create/global/project.rs
+++ src/create/global/project.rs
@@ -5,12 +5,38 @@ use indicatif::{ProgressBar, ProgressStyle};
 
 use crate::cli::{command, done, msg};
 
-fn in_windons(path: &str) {
-  command("cmd", ["/C", "rmdir", "/s", "/q", ".git"].to_vec(), Some(path), Some("Failed to reset git"));
+fn create_url(repository: &str) -> String {
+  if cfg!(target_os = "windows") {
+    format!("https://github.com/Platimex/{repository}.git")
+  } else {
+    format!("git@github.com:Platimex/{repository}.git")
+  }
 }
 
-fn in_unix(path: &str) {
-  command("rm", ["-rf", ".git"].to_vec(), Some(path), Some("Failed to reset git"));
+fn rm_git(path: &str) {
+  if cfg!(target_os = "windows") {
+    command("cmd", ["/C", "rmdir", "/s", "/q", ".git"].to_vec(), Some(path), Some("Failed to reset git"));
+  } else {
+    command("rm", ["-rf", ".git"].to_vec(), Some(path), Some("Failed to reset git"));
+  }
+}
+
+fn cp_envs(path: &str) {
+  if cfg!(target_os = "windows") {
+    command("cmd", ["/C", "copy", ".env.example", ".env.development"].to_vec(), Some(path), Some("Failed to create development env file"));
+    command("cmd", ["/C", "copy", ".env.example", ".env.production"].to_vec(), Some(path), Some("Failed to create production env file"));
+  } else {
+    command("cp", [".env.example", ".env.development"].to_vec(), Some(path), Some("Failed to create development env file"));
+    command("cp", [".env.example", ".env.production"].to_vec(), Some(path), Some("Failed to create production env file"));
+  }
+}
+
+fn install(path: &str) {
+  if cfg!(target_os = "windows") {
+    command("cmd", ["/C", "pnpm", "install"].to_vec(), Some(path), Some("Failed to install dependencies"));
+  } else {
+    command("pnpm", ["install"].to_vec(), Some(path), Some("Failed to install dependencies"));
+  }
 }
 
 pub fn make(
@@ -27,11 +53,7 @@ pub fn make(
 
   let repository = *template.get(1).unwrap();
   let commit = format!("🎉 FEAT: Starting project {name}");
-  let url = if cfg!(target_os = "windows") {
-    format!("https://github.com/Platimex/{repository}.git")
-  } else {
-    format!("git@github.com:Platimex/{repository}.git")
-  };
+  let url = create_url(repository);
 
   command(
     "git",
@@ -45,11 +67,7 @@ pub fn make(
   // }
 
   pb.set_message("Reseting git...");
-  if cfg!(target_os = "windows") {
-    in_windons(path);
-  } else {
-    in_unix(path);
-  }
+  rm_git(path);
 
   pb.set_message("Adding initial commint...");
   command("git", ["init", "-b", "main"].to_vec(), Some(path), Some("Failed to restart git"));
@@ -61,11 +79,10 @@ pub fn make(
   command("git", ["remote", "add", "template", url.as_str()].to_vec(), Some(path), Some("Failed to add remote repository"));
 
   pb.set_message("Creating env files...");
-  command("cp", [".env.example", ".env.development"].to_vec(), Some(path), Some("Failed to create development env file"));
-  command("cp", [".env.example", ".env.production"].to_vec(), Some(path), Some("Failed to create production env file"));
+  cp_envs(path);
 
   pb.set_message("Instaling dependencies...");
-  command("pnpm", ["install"].to_vec(), Some(path), Some("Failed to install dependencies"));
+  install(path);
   pb.finish_and_clear();
 
   done();
